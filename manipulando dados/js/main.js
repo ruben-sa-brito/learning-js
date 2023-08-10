a2eb74e870e1cf302179d18779abdb07772175a8
Timewound <rubenjms1@gmail.com>
2023-08-10 10:54:40 -0300

    salvando dados no local storage

--- manipulando dados/js/main.js
+++ manipulando dados/js/main.js
@@ -1,10 +1,17 @@
 const form = document.getElementById("novoItem")
 const lista = document.getElementById("lista")
+const itens = []
 
 form.addEventListener("submit", (evento)=> {
     evento.preventDefault()
-    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
-    
+
+    const nome = evento.target.elements['nome']
+    const quantidade = evento.target.elements['quantidade']
+
+    criaElemento(nome.value, quantidade.value)
+
+    nome.value = ""
+    quantidade.value = ""
 })
 
 
@@ -22,6 +29,15 @@ function criaElemento(nome, quantidade) {
     novoItem.innerHTML += nome
 
     lista.appendChild(novoItem)
+
+    const itemAtual = {
+        "nome":nome,
+        "quantidade": quantidade
+    }
+
+    itens.push(itemAtual)
+    localStorage.setItem("item", JSON.stringify(itens))
+    
 
 
 }
