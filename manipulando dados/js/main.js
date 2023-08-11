6970c04b9f4e2fb46ccf0d97ace03baaa2361316
Timewound <rubenjms1@gmail.com>
2023-08-11 09:27:54 -0300

    recuperando itens do local storage

--- manipulando dados/js/main.js
+++ manipulando dados/js/main.js
@@ -1,6 +1,24 @@
 const form = document.getElementById("novoItem")
 const lista = document.getElementById("lista")
-const itens = []
+const itens =  JSON.parse(localStorage.getItem("item")) || []
+
+
+itens.forEach((item)=>{
+    
+    const novoItem = document.createElement('li')
+    novoItem.classList.add('item')
+
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = item["quantidade"]
+
+    novoItem.appendChild(numeroItem)
+    novoItem.innerHTML += item["nome"]
+
+    lista.appendChild(novoItem)
+    
+
+})
+
 
 form.addEventListener("submit", (evento)=> {
     evento.preventDefault()
